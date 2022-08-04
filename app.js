7a56826d0c9a14a65e1b7975b3b04c0e5ab00c8e
sokret sathit <sokretsathit13@gmail.com>
2022-08-04 14:17:52 -0500

    Fixed diet issues

--- app.js
+++ app.js
@@ -11,7 +11,9 @@ function formAlert() {
     const age = form.age.value;
     const gender = form.gender.value;
     const location = form["travel-location"].value;
-    let diet = "";
+    const diet = [""];
+
+
     if (form.vegan.checked) {
         diet.push(document.getElementById('vegan').value);
     }
@@ -22,55 +24,10 @@ function formAlert() {
         diet.push(document.getElementById('paleo').value);
     }
 
-    // newWindow = window.open("", "_blank");
-    alert("First Name: " + firstName + "\nLast Name: " + lastName + "\nAge: " + age + "\nGender: " + gender + "\nTravel Location: " + location + "\nDiet: " + diet + "\nAwesome, now if you die, it won't be an accident..");
+    alert("First Name: " + firstName + "\nLast Name: " + lastName + "\nAge: " + age + "\nGender: " + gender + "\nTravel Location: " + location  + "\nDiet:" + diet + "\nAwesome, now if you die, it won't be an accident..");
 }
 
 form.addEventListener("submit", function(e){
     e.preventDefault()
 	formAlert()
 })
-
-// var popup = Runner.displayPopup( {
-//     html: "<h1>Hello world!</h1><p>It works</p>",
-//     header: 'Custom popup text'
-// });
-
-
-
-// form.addEventListener("submit", function(formAlert){
-//     formAlert.preventDefault();
-
-// })
-
-
-
-
-
-    // DispWin = window.open('','NewWin', 'toolbar=no,status=no,width=500,height=700')
-    
-    // message = "<ul><li><b>FirstName: </b>" + firstName;
-    // message += "<li><b>LastName: </b>" + lastName;
-    // message += "<li><b>Age: </b>" + age;
-    // message += "<li><b>Gender: </b>" + gender;
-    // message += "<li><b>Location: </b>" + location;
-    // message += "<li><b>Diet: </b>" + diet; + "</ul>";
-    // DispWin.document.write(message);
-
-
-
-// submit.addEventListener('click', formAlert);
-  
-
-// document.querySelector('.form')submit.addEventListener('click', function(){
-//         formAlert.preventDefault();   
-// });
-
-// addEventListener('submit', (event) => {});
-
-// onsubmit = (event) => { };
-
-// document.getElementById('airline-form').addEventListener('submit', function(formAlert){
-//     formAlert.preventDefault();
-
-// })
